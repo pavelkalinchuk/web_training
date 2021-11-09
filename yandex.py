dda580d7fb1e390f01d106d9280fdf0e26a3ba6d
pavelkalinchuk <715m4s@gmail.com>
2021-11-09 23:38:34 +0300

    Реализовано сохранение полученной информации в json-файл

--- yandex.py
+++ yandex.py
@@ -1,10 +1,13 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 import time
+import json
 
 wd = Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
 wd.implicitly_wait(30)
 wd.get("http://yandex.ru")
+
+data = {}
 
 try:
     wd.find_element(By.XPATH, '//*[@id="text"]').click()
@@ -13,8 +16,12 @@ try:
     wd.find_element(By.XPATH, '//button[@type="submit"]').click()
     hrefs = wd.find_elements(By.XPATH, "//li[@class='serp-item desktop-card']//a[@accesskey]")
     for elem in hrefs:
-        print (">> Text: " + elem.text + "\n    URL: " + elem.get_attribute('href'))
-    #print(hrefs)
+        data[elem.text] = elem.get_attribute('href')
     time.sleep(3)
+    data_json = json.dumps(data, ensure_ascii = False, indent = 4)
+    #print(data_json)
+    with open("data.json", "w") as f:
+        f.write(data_json)
 finally:
     wd.close()
+
