b282457c4422c39f2ff52ae2f44370b0cfb6b73e
pavelkalinchuk <715m4s@gmail.com>
2021-11-09 19:48:17 +0300

    update

--- yandex.py
+++ yandex.py
@@ -1,12 +1,20 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 import time
 
-wd = webdriver.Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
+wd = Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
 wd.implicitly_wait(30)
 wd.get("http://yandex.ru")
-wd.find_element_by_xpath('//*[@id="text"]').click()
-wd.find_element_by_xpath('//*[@id="text"]').clear()
-wd.find_element_by_xpath('//*[@id="text"]').send_keys("test")
-wd.find_element_by_xpath("//button[@type='submit']").click()
-time.sleep(3)
-wd.close()
+
+try:
+    wd.find_element(By.XPATH, '//*[@id="text"]').click()
+    wd.find_element(By.XPATH, '//*[@id="text"]').clear()
+    wd.find_element(By.XPATH, '//*[@id="text"]').send_keys("test")
+    wd.find_element(By.XPATH, '//button[@type="submit"]').click()
+    hrefs = wd.find_elements(By.XPATH, "//li[@class='serp-item desktop-card']//a[@accesskey]")
+    for elem in hrefs:
+        print (">> Text: " + elem.text + "\n    URL: " + elem.get_attribute('href'))
+    #print(hrefs)
+    time.sleep(3)
+finally:
+    wd.close()
