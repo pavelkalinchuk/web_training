4544413de7f0615391cb39c1208f09cec19bfaaf
pavelkalinchuk <715m4s@gmail.com>
2024-04-18 21:06:49 +0300

    Сохраняем новые доработки

--- yandex.py
+++ yandex.py
@@ -2,25 +2,39 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 import json
 
-i = input("Введите что надо найти: ")
+result_file = 'results/yandex.json'
 data = {}
 
-wd = Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
-wd.implicitly_wait(30)
-wd.get("http://yandex.ru")
+i = input("Введите что надо найти: ")
 
-try:
-    wd.find_element(By.XPATH, '//*[@id="text"]').click()
-    wd.find_element(By.XPATH, '//*[@id="text"]').clear()
-    wd.find_element(By.XPATH, '//*[@id="text"]').send_keys(i)
-    wd.find_element(By.XPATH, '//button[@type="submit"]').click()
-    hrefs = wd.find_elements(
-        By.XPATH, "//li[@class='serp-item desktop-card']//a[@accesskey]")
-    for elem in hrefs:
-        data[elem.text] = elem.get_attribute('href')
-    data_json = json.dumps(data, ensure_ascii=False, indent=4)
-    # print(data_json)
-    with open("save.json", "w") as f:
-        f.write(data_json)
-finally:
-    wd.close()
+wd = Chrome()
+wd.fullscreen_window()
+wd.get("http://yandex.ru")
+# Ниже записываем в переменную 'original_handle' идентификатор текущей вкладки
+original_handle = wd.current_window_handle
+
+# Так как строка поиска находится во фрэйме, то первой строкой делаем переход в него
+wd.switch_to.frame(wd.find_element(By.CSS_SELECTOR,"iframe"))
+wd.find_element(By.CSS_SELECTOR, 'form.arrow').click()
+wd.find_element(By.CSS_SELECTOR, '.arrow__input').send_keys(i)
+wd.find_element(By.XPATH, '//button[@type="submit"]').click()
+
+# Учитывая то, что результат поиска выводится на новую вкладку, то делаем переход на эту вкладку
+for window_handle in wd.window_handles:
+        if window_handle != original_handle:
+                wd.switch_to.window(window_handle)
+
+hrefs = wd.find_elements(
+        By.CSS_SELECTOR, 'a[accesskey]')
+
+for elem in hrefs:
+    head = elem.get_attribute('innerText')
+    href = elem.get_attribute('href')
+    data[head] = href
+# print(data)
+data_json = json.dumps(data, ensure_ascii=False, indent=4)
+# print(data_json)
+with open(result_file, "w+") as f:
+    f.write(data_json)
+
+wd.close()
